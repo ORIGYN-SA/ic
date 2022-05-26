dac4d7b365058201e6a1d00d182ec102c144265a
origyndev <austin.fatheree+alt@gmail.com>
2022-05-26 00:28:51 -0500

    fixing first block

--- rs/rosetta-api/build-archive.sh
+++ rs/rosetta-api/build-archive.sh
@@ -3,5 +3,5 @@ set -euo pipefail
 
 cargo build --target wasm32-unknown-unknown --release --bin ledger-canister/archive_node -p ledger-canister
 # This is the worlds most primitive way of doing tree shaking, but it trims 18MB of the size of the canister
-wasm2wat ../target/wasm32-unknown-unknown/release/ledger-archive-node-canister.wasm -o ../target/wasm32-unknown-unknown/release/ledger-archive-node-canister.wasm.wat
-wat2wasm ../target/wasm32-unknown-unknown/release/ledger-archive-node-canister.wasm.wat -o ../target/wasm32-unknown-unknown/release/ledger-archive-node-canister.wasmr-min.wasm
+wasm2wat ../target/wasm32-unknown-unknown/release/ledger-archive-node-canister.wasm -o ../target/wasm32-unknown-unknown/release/ledger-archive-node-canister.wasm20220525fix.wat
+wat2wasm ../target/wasm32-unknown-unknown/release/ledger-archive-node-canister.wasm20220525fix.wat -o ../target/wasm32-unknown-unknown/release/ledger-archive-node-canister.wasmr-min20220525fix.wasm
--- rs/rosetta-api/ledger_canister/src/archive_node.rs
+++ rs/rosetta-api/ledger_canister/src/archive_node.rs
@@ -1,4 +1,4 @@
-use ic_base_types::PrincipalId;
+
 use ledger_canister::{
     metrics_encoder::MetricsEncoder, BlockHeight, BlockRange, BlockRes, CandidBlock, EncodedBlock,
     GetBlocksArgs, GetBlocksError, GetBlocksResult, IterBlocksArgs, MAX_BLOCKS_PER_REQUEST,
@@ -6,8 +6,8 @@ use ledger_canister::{
 
 use candid::{candid_method, Nat};
 use dfn_candid::candid_one;
-use dfn_core::api::{print, stable_memory_size_in_pages};
-use dfn_core::{over_init, stable, BytesS};
+use dfn_core::api::{print, stable_memory_size_in_pages,};
+use dfn_core::{over_init, stable, BytesS, over};
 use dfn_protobuf::protobuf;
 use serde::{Deserialize, Serialize};
 use std::sync::RwLock;
@@ -151,17 +151,20 @@ fn main() {
     )
 }
 
-#[export_name = "canister_update fix_first_block"]
-fn fix_first_block() {
-    let accepted_principal = PrincipalId::from_str("6frco-ivjsx-htvpq-7pc6h-uzesu-6io4p-yj6rv-xjpst-prjjg-hsqxs-oae").unwrap();
-    assert_eq(dfn_core::api::caller(), accepted_principal); //check if the caller is the one we expect
-
-    let archive_state = ARCHIVE_STATE.write().unwrap();
+#[candid_method(update, rename = "fix_first_block")]
+fn fix_first_block() -> () {
+    /* let mut archive_state = ARCHIVE_STATE.write().unwrap();
     let first_block = archive_state.blocks.get_mut(0).unwrap(); //get the first block (block 0) for mutation
     let mut first_block_decoded = first_block.decode().unwrap(); //decode first block
     first_block_decoded.parent_hash = None; //set parent hash of first block to None
-    *first_block = first_block_decoded.encode().unwrap(); //re-encode first block and set it to the archive state
+    *first_block = first_block_decoded.encode().unwrap(); //re-encode first block and set it to the archive state */
 }
+
+#[export_name = "canister_update fix_first_block"]
+fn fix_first_block_candid() {
+    over(candid_one, |()|fix_first_block())
+}
+
 
 #[export_name = "canister_update remaining_capacity"]
 fn remaining_capacity_() {
